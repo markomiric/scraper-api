335ce3cd15f8582e84333e1b75ee06bf17e1a239
markomiric <marko_miric@outlook.com>
2024-12-07 02:58:28 +0100

    feat: add first test and health endpoint

---
+++ services/backend/tests/test_health.py
@@ -0,0 +1,16 @@
+import pytest
+from fastapi import status
+from starlette.testclient import TestClient
+
+from main import app
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+def test_health(client):
+    response = client.get("/api/health")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"message": "OK"}
