4d403eeab18a367d699184a0243a0cbc938556cc
markomiric <marko_miric@outlook.com>
2024-12-13 09:17:19 +0100

    feat: add additional permissions and env variables for cognito

--- services/backend/src/config.py
+++ services/backend/src/config.py
@@ -2,14 +2,23 @@ import logging
 from functools import lru_cache
 from typing import Optional
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 log = logging.getLogger("uvicorn")
 
 
 class Settings(BaseSettings):
-    TABLE_NAME: str = ""
     DYNAMODB_URL: Optional[str] = None
+    TABLE_NAME: str = ""
+    AWS_DEFAULT_REGION: str
+    AWS_USER_POOL_ID: str
+    AWS_USER_POOL_CLIENT_ID: str
+
+    model_config = SettingsConfigDict(
+        env_file=(".env", ".env.development"),
+        _env_file_encoding="utf-8",
+        extra="ignore",
+    )
 
 
 @lru_cache()
